cc06273ddeb3ff14faa5106e480bff95d45c4e87
Tbwilson89 <trevorbwilson89@gmail.com>
2018-03-20 23:11:16 -0500

    working on drag and drop feature in test environment

--- src/components/projects/cardgame/card.js
+++ src/components/projects/cardgame/card.js
@@ -7,13 +7,15 @@ export default class Card extends Component {
 
   render(){
     let Card = this.props.cardSize === 'full' ? CardFull : CardMini
+    console.log(this.props.data)
     return(
-      <div style={{height: '100%'}}>
+      <div style={{height: '100%', margin: '0'}}>
         <Card
           loc={this.props.loc}
           data={this.props.cardData}
           onClick={this.props.handleClick}
           onHover={this.props.onHover}
+          onDrag={this.props.onDrag}
         />
       </div>
     )
--- src/components/projects/testmoving.js
+++ src/components/projects/testmoving.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react'
 
+import cardInfo from './cardgame/database/cardinfo'
+import Card from './cardgame/card'
+
 export default class testMove extends Component {
   constructor(props){
     super(props)
@@ -16,82 +19,47 @@ export default class testMove extends Component {
           y: 0,
         }
       ],
-      sectionTwo: ['square', 'square'],
-      currentLoc: '',
-      currentPos: '',
+      showHand: false,
+      playerDeck: []
     }
-    this.MouseDown = this.MouseDown.bind(this)
-    this.whenMouseMove = this.whenMouseMove.bind(this)
-    this.mouseUp = this.mouseUp.bind(this)
     this.drag = this.drag.bind(this)
     this.allowDrop = this.allowDrop.bind(this)
     this.drop = this.drop.bind(this)
+    this.showHand = this.showHand.bind(this)
+    this.drawCard = this.drawCard.bind(this)
+  }
+
+  drawCard(){
+    document.getElementById('player-hand').appendChild(document.getElementById('player-deck').children[0])
+    console.log(this.state.playerDeck)
+  }
+  showHand(){
+    this.setState({showHand: true})
   }
 
   allowDrop(e){
     e.preventDefault()
   }
   drag(e){
-    console.log(e.dataTransfer.getData('test') + ' ' + e.target)
-    e.dataTransfer.setData('text', e.target.id)
+    console.log('test')
+    e.dataTransfer.setData('text', e.currentTarget.id)
   }
   drop(e){
     e.preventDefault()
+    console.log(e.dataTransfer.getData('test'))
     var data = e.dataTransfer.getData('text')
-    e.target.appendChild(document.getElementById(data))
+    e.currentTarget.appendChild(document.getElementById(data))
   }
 
   componentDidMount(){
-    // this.state.sectionOne.forEach((s,i)=>{
-    //   this.dragElement(`secone${i}}`)
-    // })
-  }
-  MouseDown(e){
-    let loc = e.currentTarget.id.split('').map((l)=>isNaN(l) ? l : '').join('')
-    let pos = e.currentTarget.id.split('').map((l)=>isNaN(l) ? '' : l).join('')
-
-    console.log(loc + ' ' + pos)
-    let section = JSON.parse(JSON.stringify(this.state[loc]))
-    section[pos].dragging = true
-    section[pos].x = e.clientX
-    section[pos].y = e.clientY
-
-    console.log(section)
+    console.log(document.getElementById('player-deck').children[0])
+    let deck = []
+    for(let i=0;i<30;i++){
+      deck.push(<div id={`card${i}`} draggable='true' onDragStart={this.drag} style={{}}><Card loc={`card${i}`} cardSize={''} cardData={cardInfo.fighter} onClick={function(){}} onHover={function(){}}/></div>)
+    }
     this.setState({
-      sectionOne: section,
-      currentLoc: loc,
-      currentPos: pos
+      playerDeck: deck
     })
-  }
-  mouseUp(e){
-    if(this.state.currentLoc !== ''){
-      let section = JSON.parse(JSON.stringify(this.state[this.state.currentLoc]))
-      section[this.state.currentPos].dragging = false;
-
-      console.log(section)
-      this.setState({
-        sectionOne: section,
-        currentLoc: '',
-        currentPos: ''
-      })
-
-    }
-  }
-  mouseLeaves(e){
-
-  }
-  whenMouseMove(e){
-    let posX = e.clientX
-    let posY = e.clientY
-    if(this.state.currentLoc !== ''){
-      let currentTarget = JSON.parse(JSON.stringify(this.state[this.state.currentLoc]))
-      currentTarget[this.state.currentPos].x = posX
-      currentTarget[this.state.currentPos].y = posY
-      console.log(posX + ' ' + posY)
-      this.setState({
-        sectionOne: currentTarget
-      })
-    }
   }
 
   render(){
@@ -100,8 +68,6 @@ export default class testMove extends Component {
         <div
           id={`sectionOne${i}`}
           className='square'
-          onMouseDown={this.MouseDown}
-          onMouseUp={this.mouseUp}
           // onMouseMove={this.whenMouseMove}
           style={{
             height: '100px',
@@ -116,28 +82,68 @@ export default class testMove extends Component {
     })
     // let secTwo = this.state.sectionOne.map((i)=>(<div id={`sectwo${i}`} className='square' style={{height: '100px', width: '100px',backgroundColor: 'blue'}} onClick={this.dragElement}></div>))
     return(
-      <div className='wrapper' onMouseMove={this.whenMouseMove} onMouseLeave={this.mouseUp} onMouseUp={this.mouseUp}>
-        <div className='sectionOne'>{secOne}</div>
-        <div className='sectionTwo'>{}</div>
-        <div style={{height: '150px', width: '150px', border: '1px solid black'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
-        <div style={{height: '150px', width: '150px', border: '1px solid black'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
+        <div className='wrapper'
+          style={{
+            position: 'relative',
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr',
+            gridTemplateRows: '1.25fr 2fr 2fr 1.25fr',
+            gridTemplateAreas: "'oresources oresources' 'oattack odefense''pdefense pattack' 'presources presources'",
+            gridGap: '1em',
+            padding: '1em 1em 2em 1em',
+            overflow: 'hidden'
+          }}>
+          <div className='opponent-resources' style={{backgroundColor: '#0e0', gridArea: 'oresources', display: 'grid', gridTemplateColumns: 'repeat(5,1fr)'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
+          <div className='opponent-defense' style={{backgroundColor: '#00e', gridArea: 'odefense', display: 'flex', flexWrap: 'wrap'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
+          <div className='opponent-attack' style={{backgroundColor: '#e00', gridArea: 'oattack', display: 'flex', flexWrap: 'wrap'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
 
-        <div
+          <div className='player-attack' style={{backgroundColor: '#e00', gridArea: 'pattack', display: 'flex', flexWrap: 'wrap'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
+          <div className='player-defense' style={{backgroundColor: '#00e', gridArea: 'pdefense', display: 'flex', flexWrap: 'wrap'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
+          <div className='player-resources' style={{backgroundColor: '#0e0', gridArea: 'presources', display: 'flex', flexWrap: 'wrap'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
+
+          <div
+            id='player-hand'
+            className='player-hand'
+            style={{
+              display: 'flex',
+              position: 'absolute',
+              bottom: this.state.showHand === true ? '150px' : '-75px',
+              width: '100%',
+              height: '100px',
+              backgroundColor: '#eee',
+              transition: 'bottom .5s'
+            }}
+            onClick={this.showHand}>
+            <button onClick={this.drawCard}>DRAW A CARD!</button>
+
+          </div>
+
+          <div id='player-deck' className='player-deck' style={{display: 'none'}}>
+            {this.state.playerDeck}
+          </div>
+
+
+          {/* <div className='sectionOne'>{secOne}</div>
+          <div className='sectionTwo'>{}</div>
+          <div style={{height: '150px', width: '150px', border: '1px solid black'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
+          <div style={{height: '150px', width: '150px', border: '1px solid black'}} onDrop={this.drop} onDragOver={this.allowDrop}></div>
+
+          <div
           id='testmover'
           draggable='true'
           onDragStart={this.drag}
-          style={{height: '100px', width: '100px', backgroundColor: 'red'}}
+          style={{minHeight: '100px', minWidth: '100px', backgroundColor: 'red'}}
           >
 
         </div>
         <div
-          id='testmover2'
-          draggable='true'
-          onDragStart={this.drag}
-          style={{height: '100px', width: '100px', backgroundColor: 'green'}}
-          >
+        id='testmover2'
+        draggable='true'
+        onDragStart={this.drag}
+        style={{height: '100px', width: '100px', backgroundColor: 'green'}}
+        >
 
-        </div>
+      </div> */}
       </div>
     )
   }
